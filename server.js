c2a34c159eeee76db083185c5596e7ad5d446e34
alexsimonslc-art <alexsimons.lc@gmail.com>
2026-03-28 11:03:37 +0530

    Fix: increase body size limit to 25mb for certificate generation

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-app.use(express.json());
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ limit: '25mb', extended: true }));
 app.use(session({
   secret: process.env.JWT_SECRET,
   resave: false,
